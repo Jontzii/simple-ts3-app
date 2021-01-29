82bc99713f6d0850b7654d4171b9be8f2abdf954
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-01-29 13:16:26 +0200

    Added spinning loader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <a
+/*
+  TODO:
+  Every channel is one tile in a grid
+
+  Every tile has a header showing the name of the channel
+  Every tile has a list of clients on that channel
+*/
+
+class App extends React.Component {
+
+  // fake loading time
+  LoadingFaker(){
+    return new Promise(resolve => setTimeout(resolve, 500)) // 1 seconds
+  }
+
+  componentDidMount(){
+    this.LoadingFaker().then(() => {
+      const spinningLoader = document.getElementById('Loader')
+      
+      if(spinningLoader){
+        // fade out
+        spinningLoader.classList.add('available')
+        setTimeout(() => {
+          // remove from DOM
+          spinningLoader.outerHTML = ''
+        }, 2000)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Naapur1t TS server
+          </h1>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <a
           className="App-link"
           target="_blank"
           rel="nofollow noopener noreferrer"
-        >
-          Join server!
-        </a>
-      </header>
-    </div>
-  );
+          >
+            Join server!
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
