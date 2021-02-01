db2596e0c2c0eaee2ee06dce791005e7fa45d286
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-02-01 19:49:20 +0200

    Added mute lookup

--- src/ChannelRender/SingleClient.js
+++ src/ChannelRender/SingleClient.js
@@ -4,6 +4,12 @@ export default class RenderSingleClient extends React.Component {
   render() {
     const { client } = this.props;
     
+    if (client.clientInputMuted || client.clientOutputMuted) {
+      return (
+        <li style={{color: "red"}}>{client.clientNickname}</li>
+      )
+    }
+
     return (
       <li>{client.clientNickname}</li>
     )
