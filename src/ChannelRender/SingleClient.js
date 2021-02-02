772bdf45a262667a314e583d6a40757cc6f9afbd
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-02-02 02:22:21 +0200

    Added better icons for mute & deafen

--- src/ChannelRender/SingleClient.js
+++ src/ChannelRender/SingleClient.js
@@ -1,17 +1,51 @@
 import React from 'react'
 
+/* Icons */
+import arrow from './../Assets/arrow.png'
+import inputOff from './../Assets/inputOff.png'
+import outputOff from './../Assets/outputOff.png'
+
 export default class RenderSingleClient extends React.Component {
   render() {
     const { client } = this.props;
-    
-    if (client.clientInputMuted || client.clientOutputMuted) {
+
+    if (client.clientInputMuted && client.clientOutputMuted) {
       return (
-        <li style={{color: "red"}}>{client.clientNickname}</li>
+        <li>
+          <span>
+            <img src={arrow} alt="Arrow" />
+            <span> {client.clientNickname} - <img src={inputOff} alt="Input muted" /> | <img src={outputOff} alt="Output muted" /></span>
+          </span>
+        </li>
       )
     }
-
-    return (
-      <li>{client.clientNickname}</li>
-    )
+    else if (client.clientInputMuted && !client.clientOutputMuted) {
+      return (
+        <li>
+          <span>
+            <img src={arrow} alt="Arrow" />
+            <span> {client.clientNickname} - <img src={inputOff} alt="Input muted" /></span>
+          </span>
+        </li>
+      )
+    }
+    else if (!client.clientInputMuted && client.clientOutputMuted) {
+      return (
+        <li>
+          <span>
+            <img src={arrow} alt="Arrow" />
+            <span> {client.clientNickname} - <img src={outputOff} alt="Output muted" /></span>
+          </span>
+        </li>
+      )
+    }
+    else {
+      return (
+        <li>
+          <img src={arrow} alt="Arrow" />
+          <span> {client.clientNickname}</span>
+        </li>
+      )
+    }
   }
 }
