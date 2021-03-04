30b3360f886dd2448d5ab6539446b6eea0e26d14
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-03-05 00:54:16 +0200

    Add support for errors
    - Make data null when fetching error happens
    - Add correct height to error page

--- src/Misc/FetchData.js
+++ src/Misc/FetchData.js
@@ -2,9 +2,14 @@ const fetchData = () => {
   return new Promise((resolve, reject) => {
     // Fetch from teamspeak API
     fetch('https://api.jontzi.com/teamspeak/v1/channels')
-    .then(res => res.json())
-    .then(json => {
-      resolve(json);
+    .then(res => {
+      if (res.status === 200) {
+        res.json()
+        .then(json => resolve(json))
+      }
+      else {
+        reject(res.status)
+      }
     })
     .catch(err => reject(err))
   })
